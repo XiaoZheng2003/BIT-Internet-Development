51eff6c9a45e0a7bcffaaf81b5095f80ee2cc4e1
XiaoZheng2003 <1345486584@qq.com>
2022-08-24 21:20:35 +0800

    目录添加平滑过渡的效果

--- chap10/js/catalog.js
+++ chap10/js/catalog.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     a = $(window).height();    //浏览器窗口高度
     $(window).scroll(function(){
         b = $(this).scrollTop();   //页面滚动的高度
-        if(a+b>1200){              //滚动到该高度进行操作
+        if(b>a){              //滚动到该高度进行操作
             var head=document.getElementById("catalog_normal");
             if(head!=null)
                 head.id="catalog_float";
@@ -13,4 +13,11 @@ $(document).ready(function(){
                 head.id="catalog_normal";
         }
     });
+});
+
+jQuery(document).ready(function($) {
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$('[name="'+this.hash.substring(1)+'"]').offset().top}, 500);
+	});
 });
