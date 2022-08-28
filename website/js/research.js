50ba81bcda074d6fd0e147c8231242614f7faf09
XiaoZheng2003 <1345486584@qq.com>
2022-08-28 21:51:56 +0800

    完善调查系统

---
+++ website/js/research.js
@@ -0,0 +1,34 @@
+function init(){
+    let un=localStorage['current-user'];
+    let research_tmp=new Array();
+    for(let i=0;i<4;i++){
+        research_tmp[i]=0;
+    }
+    let research_data=JSON.stringify(research_tmp);
+    sessionStorage.setItem('rsh_'+un,research_data);
+}
+
+function mark(id){
+    let un=localStorage['current-user'];
+    let research_tmp=JSON.parse(sessionStorage.getItem("rsh_"+un));
+    research_tmp[id]=1;
+    let research_data=JSON.stringify(research_tmp);
+    sessionStorage.setItem('rsh_'+un,research_data);
+}
+
+function showrsh(){
+    let un=localStorage['current-user'];
+    let research_tmp=JSON.parse(sessionStorage.getItem("rsh_"+un));
+    let cnt=4;
+    for(let i=0;i<4;i++){
+        if(research_tmp[i]){
+            let object=document.getElementById("research"+i);
+            object.style="display:none;";
+            cnt--;
+        }
+    }
+    if(!cnt){
+        let object=document.getElementById("next");
+        object.style="";
+    }
+}
