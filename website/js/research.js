12f89d8eeffb571eb9304df2ec37be6b8d6cc56f
XiaoZheng2003 <1345486584@qq.com>
2022-08-30 12:09:24 +0800

    完善调查部分

--- website/js/research.js
+++ website/js/research.js
@@ -1,38 +1,51 @@
-function check(){
+var scene_num=[4,3];
+
+function check(scene){
     let un=localStorage['current-user'];
-    if(!sessionStorage['rsh_'+un]){
-        init();
+    if(!sessionStorage['rsh'+scene+un]){
+        init(scene);
     }
 }
 
-function init(){
+function init(scene){
     let un=localStorage['current-user'];
     let research_tmp=new Array();
-    for(let i=0;i<4;i++){
+    for(let i=0;i<scene_num[scene];i++){
         research_tmp[i]=0;
     }
     let research_data=JSON.stringify(research_tmp);
-    sessionStorage.setItem('rsh_'+un,research_data);
+    sessionStorage.setItem('rsh'+scene+un,research_data);
 }
 
-function mark(id){
+function mark(id,scene){
     let un=localStorage['current-user'];
-    let research_tmp=JSON.parse(sessionStorage.getItem("rsh_"+un));
+    let research_tmp=JSON.parse(sessionStorage.getItem("rsh"+scene+un));
     research_tmp[id]=1;
     let research_data=JSON.stringify(research_tmp);
-    sessionStorage.setItem('rsh_'+un,research_data);
+    sessionStorage.setItem('rsh'+scene+un,research_data);
 }
 
-function showrsh(){
+function showrsh(scene){
     let un=localStorage['current-user'];
-    let research_tmp=JSON.parse(sessionStorage.getItem("rsh_"+un));
-    let cnt=4;
-    for(let i=0;i<4;i++){
+    let research_tmp=JSON.parse(sessionStorage.getItem("rsh"+scene+un));
+    let cnt=scene_num[scene];
+    for(let i=0;i<scene_num[scene];i++){
         if(research_tmp[i]){
             let object=document.getElementById("research"+i);
-            object.style="display:none;";
+            object.innerHTML='√ '+object.innerHTML;
             cnt--;
         }
+    }
+    //第一幕尸体强制优先调查
+    if(!scene&&!research_tmp[0]){
+        for(let i=1;i<scene_num[0];i++){
+            let object=document.getElementById("research"+i);
+            object.style="display:none;";
+        }
+    }
+    if(scene==1&&!research_tmp[0]){
+        let object=document.getElementById("research2");
+        object.style="display:none;";
     }
     if(!cnt){
         let object=document.getElementById("next");
