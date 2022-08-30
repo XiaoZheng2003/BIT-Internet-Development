6d72653cab6738270e44dc9de5a3e9de9b72e89a
XiaoZheng2003 <1345486584@qq.com>
2022-08-30 20:17:22 +0800

    完善成就系统

--- website/js/achievement.js
+++ website/js/achievement.js
@@ -2,7 +2,7 @@ function showAchv(){
     let un=localStorage['current-user'];
     let user_data=JSON.parse(localStorage.getItem("gv_"+un));
     let achv_data=JSON.parse(user_data['achievement_data']);
-    for(let i=0;i<4;i++){
+    for(let i=0;i<6;i++){
         if(achv_data[i]){
             let object=document.getElementById("trophy"+i);
             object.src="../img/trophy.png";
@@ -11,6 +11,7 @@ function showAchv(){
 }
 
 function achieve(id){
+    
     let un=localStorage['current-user'];
     let user_data=JSON.parse(localStorage.getItem("gv_"+un));
     let achv_data=JSON.parse(user_data['achievement_data']);;
