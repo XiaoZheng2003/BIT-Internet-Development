128fdc7425fc68da859084f4bd2ab4ef90aa2f57
XiaoZheng2003 <1345486584@qq.com>
2022-08-31 11:55:40 +0800

    修改结局，添加成就

--- website/js/achievement.js
+++ website/js/achievement.js
@@ -2,7 +2,7 @@ function showAchv(){
     let un=localStorage['current-user'];
     let user_data=JSON.parse(localStorage.getItem("gv_"+un));
     let achv_data=JSON.parse(user_data['achievement_data']);
-    for(let i=0;i<6;i++){
+    for(let i=0;i<7;i++){
         if(achv_data[i]){
             let object=document.getElementById("trophy"+i);
             object.src="../img/trophy.png";
