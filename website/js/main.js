fa6feea261cc4c2a03f3953892a31dea43202225
XiaoZheng2003 <1345486584@qq.com>
2022-09-03 12:22:08 +0800

    调整剧情中立绘

--- website/js/main.js
+++ website/js/main.js
@@ -53,10 +53,9 @@ function changeBackground(cnt,aim,url) {
 }
 
 //present赋给cnt,想改变的文本的索引赋给aim,想改变的区域赋给characterid,图片源赋给charactersrc，opa表示opacity
-function changeCharacter(cnt,aim,characterid,charactersrc,opa) {
-    if(cnt/2==aim||(cnt-1)/2==aim){
-        let cha=document.getElementById(characterid)
-        cha.src=charactersrc
-        cha.style.opacity=opa
+function changeCharacter(cnt,aim,charactersrc) {
+    if(cnt==aim*2+1){
+        let cha=document.getElementById('character1');
+        cha.src=charactersrc;
     }
 }
